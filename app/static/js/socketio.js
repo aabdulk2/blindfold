6c47430ee475662215a524cdb69ac451ad59384c
Albara Abdulkarim <90721001+aabdulk2@users.noreply.github.com>
2022-04-12 04:17:01 -0400

    Presentation

--- app/static/js/socketio.js
+++ app/static/js/socketio.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=> {
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
     
+    send_message = socket.send(document.querySelector('.message-box').value);
 }) 
